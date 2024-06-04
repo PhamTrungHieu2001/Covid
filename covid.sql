ab64f6b382bceef3ea190f5c54eaa1b18a058709
HieuPham <phamhieu2001cg@gmail.com>
2024-06-04 18:40:01 +0700

    Update covid.sql

--- covid.sql
+++ covid.sql
@@ -1,89 +1,77 @@
-select * 
-from PortfolioProject.dbo.CovidDeaths 
-where continent is null
-order by location, date
+-- 1. EXPLORE THE DATA FROM 2 TABLES
+SELECT iso_code, continent, location, date, population, total_cases, new_cases, total_deaths, new_deaths
+FROM     PortfolioProject.dbo.CovidDeaths
+WHERE  (continent IS NULL)
+ORDER BY location, date
 
-select * 
-from PortfolioProject.dbo.CovidVaccinations 
-order by location, date
+SELECT iso_code, continent, location, date, new_tests, total_tests, total_vaccinations, new_vaccinations
+FROM     PortfolioProject.dbo.CovidVaccinations
+ORDER BY location, date
 
-select location, date, new_cases, total_cases, total_deaths 
-from PortfolioProject.dbo.CovidDeaths 
-order by location, date
+	
+-- 2. NUMBER OF DEATHS AND CASES ON THE FINAL DAY OF EACH COUNTRY
+SELECT location, MAX(total_deaths) AS final_deaths, MAX(total_cases) AS final_cases, MAX(total_deaths) / MAX(total_cases) * 100 AS final_death_rate
+FROM     PortfolioProject.dbo.CovidDeaths
+GROUP BY location
+ORDER BY location
 
+	
+-- 3. TOTAL CASES AND TOTAL DEATHS IN A SPECIFIC COUNTRY EVERY DAY
+SELECT location, date, total_cases, total_deaths, total_deaths / total_cases * 100 AS death_rate
+FROM     PortfolioProject.dbo.CovidDeaths
+WHERE  (location = 'Vietnam')
+ORDER BY date
 
--- final deaths vs cases
-select location, max(total_deaths) final_deaths, max(total_cases) final_cases,  max(total_deaths)/max(total_cases)*100 final_death_rate
-from CovidDeaths 
-group by location
-order by location
+	
+-- 4. A SPECIFIC COUNTRY'S DAILY INFECTION RATE (= TOTAL CASES / POPULATION)
+SELECT location, date, total_cases, population, total_cases / population * 100 AS infection_rate
+FROM     PortfolioProject.dbo.CovidDeaths
+WHERE  (location = 'Vietnam')
+ORDER BY date
 
--- total cases vs total deaths in a country
-select location, date, total_cases, total_deaths, (total_deaths/total_cases)*100 death_rate
-from CovidDeaths
-where location = 'Vietnam'
-order by date
+	
+-- 5. COUNTRIES WITH THE HIGHEST INFECTION RATE (= TOTAL CASES / POPULATION)
+SELECT location, population, MAX(total_cases) AS final_cases, MAX(total_cases) / population * 100 AS infection_rate
+FROM     PortfolioProject.dbo.CovidDeaths
+GROUP BY location, population
+ORDER BY infection_rate DESC, location
 
+	
+-- 6. COUNTRIES WITH THE HIGHEST NUMBER OF DEATHS PER POPULATION
+SELECT location, population, MAX(CAST(total_deaths AS int)) AS final_deaths, MAX(total_deaths) / population * 100 AS death_rate
+FROM     PortfolioProject.dbo.CovidDeaths
+WHERE  (continent IS NOT NULL)
+GROUP BY location, population
+ORDER BY final_deaths DESC, death_rate DESC
 
--- Looking at a country's infection rate day by day (total cases / population)
-select location, date, total_cases, population, (total_cases/population)*100 infection_percentage
-from CovidDeaths
-where location = 'Vietnam'
-order by date
+	
+-- 7. CONTINENTS WITH THE HIGHEST NUMBER OF DEATHS
+SELECT continent, MAX(CAST(total_deaths AS int)) AS final_deaths
+FROM     PortfolioProject.dbo.CovidDeaths
+WHERE  (continent IS NOT NULL)
+GROUP BY continent
+ORDER BY final_deaths DESC
 
+	
+-- 8. TOTAL NUMBER OF CASES, TOTAL NUMBER OF DEATHS, AND DEATH RATE IN THE WORLD
+SELECT SUM(new_cases) AS global_cases, SUM(CAST(new_deaths AS int)) AS global_deaths, SUM(CAST(new_deaths AS int)) / SUM(new_cases) * 100 AS global_death_rate
+FROM     PortfolioProject.dbo.CovidDeaths
+WHERE  (continent IS NOT NULL)
 
--- Looking at countries with the highest infection rate compared to population (total cases / population)
-select location, population, MAX(total_cases) final_cases, MAX(total_cases)/population * 100 infection_rate
-from CovidDeaths
-group by location, population
-order by infection_rate desc, location
-
-
--- Looking at countries with highest deaths per population
-select location, population, MAX(cast(total_deaths as int)) final_deaths, MAX(total_deaths)/population * 100 death_rate
-from CovidDeaths
-where continent is not null
-group by location, population
-order by final_deaths desc, death_rate desc
-
-
--- Looking at continents with highest deaths
-select continent, MAX(cast(total_deaths as int)) final_deaths
-from CovidDeaths
-where continent is not null
-group by continent
-order by final_deaths desc
-
-
--- Looking at the global numbers
-select sum(new_cases) global_cases, sum(cast(new_deaths as int)) global_deaths, sum(cast(new_deaths as int))/sum(new_cases) * 100 global_death_rate
-from CovidDeaths
-where continent is not null
-
-
--- Vaccination rate around the world day by day
-with VaccinationPopulation
-as (
-select d.continent, d.location, d.date, d.population, v.new_vaccinations
-, sum(CONVERT(int, new_vaccinations)) over (partition by d.location order by d.date) total_vaccinations
-from PortfolioProject.dbo.CovidDeaths d
-join PortfolioProject.dbo.CovidVaccinations v 
-	on d.location = v.location
-	and d.date = v.date
-where d.continent is not null
+	
+-- 9. VACCINATION RATE AROUND THE WORLD EVERY DAY
+WITH VaccinationPopulation
+AS (
+	SELECT d.continent, d.location, d.date, d.population, v.new_vaccinations, sum(CONVERT(int, new_vaccinations)) OVER 
+		(partition by d.location ORDER BY  d.date) total_vaccinations
+	FROM PortfolioProject.dbo.CovidDeaths d
+	JOIN PortfolioProject.dbo.CovidVaccinations v 
+		ON d.location = v.location
+		AND d.date = v.date
+	WHERE d.continent is NOT null
 )
 
-select *, total_vaccinations/population * 100 vaccination_rate
-from VaccinationPopulation
-order by location, date
-
--- Create Views for visualization
-create view VaccinationPopulation
-as 
-select d.continent, d.location, d.date, d.population, v.new_vaccinations
-, sum(CONVERT(int, new_vaccinations)) over (partition by d.location order by d.location, d.date) total_vaccinations
-from PortfolioProject.dbo.CovidDeaths d
-join PortfolioProject.dbo.CovidVaccinations v 
-	on d.location = v.location
-	and d.date = v.date
-where d.continent is not null 
+SELECT location, date, population, new_vaccinations, total_vaccinations, total_vaccinations / population * 100 AS vaccination_rate
+FROM     VaccinationPopulation
+WHERE  (total_vaccinations IS NOT NULL)
+ORDER BY location, date
